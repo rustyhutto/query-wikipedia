ee336f445d36745cc25490032516a668773379bb
mbigras <mbigras22@gmail.com>
2016-10-16 14:27:20 -0700

    Fix index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 //open up resources to be publicly viewable
-app.use(express.static(__dirname + '/'));
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/dist'));
 
 var port = process.argv[2] || process.env.PORT || 3000;
 app.listen(port, function () {

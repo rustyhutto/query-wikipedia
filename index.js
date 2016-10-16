5142c33b782f5ee41323e04a0a0ff8e93aaa8408
mbigras <mbigras22@gmail.com>
2016-10-16 16:53:40 -0700

    Add CORS

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var express = require('express');
 var app = express();
 var https = require('https');
 
+// http://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/', function(req, res) {
     // console.log(req.query['params'])
     query = req.query['params']

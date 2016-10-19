74947919f19ac8afb2d163decf50f0d9b6ff9faf
mbigras <mbigras22@gmail.com>
2016-10-18 18:40:39 -0700

    :bug: Fix substring bug by commenting it out

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ app.use(function(req, res, next) {
 
 
 app.get('/', function(req, res) {
-  res.send('GET /query_wikipedia?params=QUERY')
+    // console.log(req.query['params'])
+    query = req.query['params']
+    // console.log("query ", query)
+
+    queryWikipedia(query, function(text) {
+        // console.log("inside cb ", text.substring(0, 15))
+        stripSpecialChars(text, function(stripped){
+        res.send(stripped)
+        })
+    })
+
+    // res.send("hello world");
 });
 
 var port = process.argv[2] || process.env.PORT || 3000;
@@ -24,7 +35,7 @@ app.listen(port, function() {
 function queryWikipedia(query, cb) {
     //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
     path = '/w/api.php?action=query&prop=extracts&exintro&explaintext&format=json&redirects&titles=' + query
-    console.log("path ", path)
+    // console.log("path ", path)
     var options = {
         host: 'en.wikipedia.org',
         path: path
@@ -38,12 +49,12 @@ function queryWikipedia(query, cb) {
         });
         //the whole response has been recieved, so we just print it out here
         response.on('end', function() {
-            console.log("inside https.request callback ", str.substring(0, 15));
-            console.log("typeof ", typeof str)
-            console.log("typeof ", typeof JSON.parse(str))
+            // console.log("inside https.request callback ", str.substring(0, 15));
+            // console.log("typeof ", typeof str)
+            // console.log("typeof ", typeof JSON.parse(str))
             obj = JSON.parse(str)
-            console.log(obj)
-            console.log("text? ", obj.query.pages[Object.keys(obj.query.pages)[0]].extract)
+            // console.log(obj)
+            // console.log("text? ", obj.query.pages[Object.keys(obj.query.pages)[0]].extract)
             text = obj.query.pages[Object.keys(obj.query.pages)[0]].extract
             cb(text)
         });
@@ -54,7 +65,7 @@ function queryWikipedia(query, cb) {
 
 function stripSpecialChars(str, cb) {
     var re = / \(.*\)/
-    console.log(str.replace(re, ""))
+    // console.log(str.replace(re, ""))
     var stripped = str.replace(re, "")
     cb(stripped)
 }
